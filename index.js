0c030181b6418b292eec44cb5e3a4cc4cd0aaa35
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2025-06-15 22:53:37 -0500

    feat: add optional pop up modal

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const modal = document.getElementByClassName('modal')
+const modal = document.getElementById('modal')
+
+setTimeout(function() {
+    modal.style.display = 'inline';
+    // console.log(modal)
+}, 1500);
